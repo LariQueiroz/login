54407b55dacf5a73dbb9c6045fb85760f60ccae7
LariQueiroz <larissa.mariz1a@gmail.com>
2026-03-03 15:09:35 -0300

    Add files via upload

---
+++ cadastro/src/main/java/com/larissa/cadastro/repositories/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.larissa.cadastro.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.larissa.cadastro.entities.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario,Long>{
+
+}
